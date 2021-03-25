c3c2079c5b5ccc168c397f04d40cbc67d592a358
Lorenzo Bernini <templarec@gmail.com>
2021-03-25 17:48:40 +0100

    Esercizio completato + bonus - added comments

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
+//INIZIO APP VUE
 var app = new Vue ({
 	el: '#root',
 	data: {
-		add: '',
-		editTodo: '',
+		add: '',								//per input di aggiunta elemento
+		editTodo: '',							//per input di modifica elemento
 		todo: [
 			{
-				id: 1,
+				id: 1,							//proprietà per rendere univoco l'oggetto per le chiamate
 				titolo: "Nutrire i gatti",
-				status: false,
-				flag: false
+				status: false,					//true: completato
+				flag: false						//true: in modifica
 			},
 			{
 				id: 2,
@@ -54,7 +55,7 @@ var app = new Vue ({
 			}
 		]
 	},
-	computed: {
+	computed: {									//computed per visualizzare i completati in fondo
 		todoComputed: function () {
 			let done = this.todo.filter((done) => done.status === true);
 			let undone = this.todo.filter((undone) => undone.status === false);
@@ -62,53 +63,53 @@ var app = new Vue ({
 		}
 	},
 	methods: {
-		aggiunta: function () {
-			let tempObj = {
-				id: getNewIdTodo(),
+		aggiunta: function () {								//metodo aggiunta in array todo
+			let tempObj = {									//oggetto temporaneo da inserire
+				id: getNewIdTodo(),							//invoca funzione che genera id successivo all'ultimo
 				titolo: this.add,
 				status: false
 			}
-			this.todo.push(tempObj);
-			this.add = '';
+			this.todo.push(tempObj);						//push in array
+			this.add = '';									//flush dell'input di inserimento
 			console.log(this.todo);
 		},
-		completato: function (index) {
+		completato: function (index) {						//metodo per todo completata
 			console.log(index);
-			this.todo.forEach(function (item) {
+			this.todo.forEach(function (item) {			//cerco l'oggetto in base all'id
 				if (item.id === index) {
-					item.status = true;
+					item.status = true;						//imposta lo stato su completato
 				}
 			})
 			console.log(this.todo);
 		},
-		modifica: function (index) {
-			this.todo.forEach(function (item) {
+		modifica: function (index) {						//metodo per modifica del titolo del todo (BONUS)
+			this.todo.forEach(function (item) {			//cerco id passato dall'html
 				if (item.id === index){
 					if (item.flag === false){
-						item.flag = true;
-					} else {
+						item.flag = true;					//toggle del flag per visualizzare o nascondere input di
+					} else {								// ... modifica
 						item.flag = false;
 					}
 				}
 			})
 		},
-		edit: function (index) {
+		edit: function (index) {							//metodo per keyup inserimento modifica
 			console.log(this.editTodo, index)
-			let tempTxt = this.editTodo;
+			let tempTxt = this.editTodo;					//salvo valore di input
 			this.editTodo = ''
-			this.todo.forEach(function (item) {
+			this.todo.forEach(function (item) {			//cerco id da modificare
 				if (item.id === index){
-					if (tempTxt != ''){
-						item.titolo = tempTxt;
-						item.flag = false;
+					if (tempTxt !== ''){
+						item.titolo = tempTxt;				//modifico il titolo
+						item.flag = false;					//nascondo input (fine modifica)
 					}
 				}
 			})
 			console.log(this.todo);
 		},
-		elimina: function (index) {
-			let rmvTemp = this.todo.findIndex( i => i.id === index);
-			this.todo.splice(rmvTemp,1);
+		elimina: function (index) {										//metodo eliminazione todo
+			let rmvTemp = this.todo.findIndex( i => i.id === index);    //cerco la posizione indice dell'id ricevuto
+			this.todo.splice(rmvTemp,1);						//cancello 1 elemento a partire dall'indice trovato
 
 
 
@@ -117,6 +118,7 @@ var app = new Vue ({
 		}
 	}
 });
+//funzione che cerca id più alto e restituisce id successivo da poter usare per nuovo oggetto
 function getNewIdTodo() {
 	let maxId = app.todo[0].id;
 	app.todo.forEach((item) => {
