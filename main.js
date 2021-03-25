b791d974bb2af44f5a9357a1ad49326e8a0befee
Lorenzo Bernini <templarec@gmail.com>
2021-03-25 15:17:56 +0100

    Layout ok

---
+++ main.js
@@ -0,0 +1,53 @@
+var app = new Vue ({
+	el: '#root',
+	data: {
+		todo: [
+			{
+				id: 1,
+				titolo: "Nutrire i gatti",
+				status: false
+			},
+			{
+				id: 2,
+				titolo: "Lavare i piatti",
+				status: false
+			},
+			{
+				id: 3,
+				titolo: "Pagare ENEL",
+				status: false
+			},
+			{
+				id: 4,
+				titolo: "Mandare mail",
+				status: false
+			},
+			{
+				id: 5,
+				titolo: "Corso Boolean",
+				status: false
+			},
+			{
+				id: 6,
+				titolo: "Fare la spesa",
+				status: false
+			},
+			{
+				id: 7,
+				titolo: "Chiamare Giulia",
+				status: false
+			},
+			{
+				id: 8,
+				titolo: "Ritirare Raccomandata",
+				status: false
+			}
+		]
+	},
+	component: {
+
+	},
+	methods: {
+
+	}
+});
